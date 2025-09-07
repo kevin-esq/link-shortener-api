e3dd56ed2c572ea72e1a5c08b0b213ea29680198
Kevin Esquivel <dev.kevinesquivel@gmail.com>
2025-09-06 21:07:48 -0500

    feat(url): implement URL shortening controller and logic

--- LinkShortener.Application/DTOs/ShortenUrlResponse.cs
+++ LinkShortener.Application/DTOs/ShortenUrlResponse.cs
@@ -5,5 +5,6 @@
         public string ShortUrl { get; set; } = String.Empty;
         public string OriginalUrl { get; set; } = String.Empty;
         public string Code { get; set; } = String.Empty;
+        public string? CreatedAt { get; set; } = String.Empty;
     }
 }
---
+++ LinkShortener.Tests/Helpers/MockRepository.cs
@@ -0,0 +1,27 @@
+﻿using LinkShortener.Application.Abstractions;
+using LinkShortener.Domain.Entities;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LinkShortener.Tests.Helpers
+{
+    public static class MockRepository
+    {
+        public static Mock<IUrlRepository> CreateDefault()
+        {
+            var mock = new Mock<IUrlRepository>();
+
+            mock.Setup(r => r.IsUnique(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+
+            mock.Setup(r => r.AddAsync(It.IsAny<ShortenedUrl>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            mock.Setup(r => r.SaveChangesAsync(It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            return mock;
+        }
+    }
+}
